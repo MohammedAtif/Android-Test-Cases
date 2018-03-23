9c545977b03b0755b3bdcc452bccb02d40f0bde4
Mohammed Atif <atif@zemosolabs.com>
2018-03-23 15:17:23 +0530

    First Commit for the project Git setup

---
+++ app/src/androidTest/java/com/zemosolabs/mindhive/androidtestapp/ExampleInstrumentedTest.java
@@ -0,0 +1,46 @@
+package com.zemosolabs.mindhive.androidtestapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("com.zemosolabs.mindhive.androidtestapp", appContext.getPackageName());
+    }
+
+    @Test
+    public void testMainActivityComponents() throws Exception {
+        MainActivity mainActivity = mainActivityTestRule.getActivity();
+        View welcomeViewRaw  = mainActivity.findViewById(R.id.welcome_content);
+        assertThat(welcomeViewRaw, notNullValue());
+        assertThat(welcomeViewRaw, instanceOf(TextView.class));
+        TextView welcomeView = (TextView) welcomeViewRaw;
+        String text = mainActivity.getResources().getString(R.string.large_text);
+        assertEquals("Checking the content", welcomeView.getText().toString(), text);
+    }
+}
---
+++ app/src/main/java/com/zemosolabs/mindhive/androidtestapp/MainActivity.java
@@ -0,0 +1,83 @@
+package com.zemosolabs.mindhive.androidtestapp;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ImageSwitcher imageSwitcher;
+    private int counter;
+    private Handler imageSwitcherHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        imageSwitcher = findViewById(R.id.switching_image);
+        Log.d(TAG, "Image view is : "+imageSwitcher);
+        imageSwitcher.setImageResource(R.drawable.scout_active);
+
+
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        fadeIn.setDuration(1000);
+        Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+        fadeOut.setDuration(1000);
+
+        imageSwitcher.setInAnimation(fadeIn);
+        imageSwitcher.setOutAnimation(fadeOut);
+
+        imageSwitcherHandler = new Handler();
+        imageSwitcherHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(counter % 5 == 0) {
+                    imageSwitcher.setImageResource(R.drawable.scout_active);
+                }else{
+                    imageSwitcher.setImageResource(R.drawable.scout_sleeping);
+                }
+                counter++;
+                imageSwitcherHandler.postDelayed(this, 2000);
+            }
+        }, 2000);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
